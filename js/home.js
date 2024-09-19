49a6ce3f868ae24e0b5325472475aeb0bf02973f
sabbirsolid <sabbir.designer21@gmail.com>
2024-09-19 18:04:41 +0600

    added home section

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('home added');
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const amountMoney = document.getElementById('add-money-input').value; //getting the amount of money
+    console.log(amountMoney);
+    const inputPin = document.getElementById('input-pin').value;
+    console.log(inputPin);
+});
